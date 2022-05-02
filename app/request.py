50175faf825f2d2c2f187d24d15ac9a04ecc4558
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-02 18:21:37 +0300

    add method to get the json response to url request

--- app/request.py
+++ app/request.py
@@ -14,3 +14,20 @@ def configure_request(app):
     source_url= app.config['NEWS_API_SOURCE_URL']
     cat_url=app.config['CAT_API_URL']
 
+def get_source():
+    '''
+    Method to get the json response to url request
+    '''
+    get_source_url= source_url.format(api_key)
+    with urllib.request.urlopen(get_source_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+    return source_results
+
