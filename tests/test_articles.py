0697792947ea64fc5e814e724deb36dab7860972
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-02 18:59:42 +0300

    extend the configurations

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -9,7 +9,7 @@ class ArticlesTest(unittest.TestCase):
         '''
         Method to run before every test 
         '''
-        self.new_article = Article('Isaac Ndirangu & Sheina hamisi', 'Trending news about gender equality and gender based violence', '2022-04-02T05:09:17Z', 'https://eige.europa.eu/gender-based-violence/what-is-gender-based-violence', 'https://images.unsplash.com/photo-1645389776061-01e3b6414546?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60', 'Gender equality')
+        self.new_article = Article('kipkurui & Evans', 'Trending news about gender equality and gender based violence', '2022-04-02T05:09:17Z', 'https://eige.europa.eu/gender-based-violence/what-is-gender-based-violence', 'https://images.unsplash.com/photo-1645389776061-01e3b6414546?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60', 'Gender equality')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article, Article))
