f53db1a2c5518570a5b79e7b82119888fb2b6e1f
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-02 16:09:01 +0300

    create configurations to main class

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     Creating configuration main class
     '''
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?language=en&apiKey={}'
-    # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
@@ -27,7 +26,6 @@ class DevConfig(Config):
 
     DEBUG = True
 
-#dictionary config_options to help us access different configuration option classes
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
